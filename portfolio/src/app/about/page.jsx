0990d54cd19cb404225611c2f70773750b1a1321
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-09-06 20:33:57 +0530

    Basic Setup Done

---
+++ portfolio/src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>I'm Tejas Nayak B</div>
+  )
+}
+
+export default page
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+        <Link href="/about">Got to About</Link>
+    </div>
+  )
+}
+
+export default Navbar
