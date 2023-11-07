3f0789ee17a0e9db05b8f25edd00a91cdd9cfbb5
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-11-07 19:18:27 +0530

    Temp about page done

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 import { Slide, Bounce } from "react-reveal";
 import Fade from "react-reveal/Fade";
 
-const Hero = () => {
+const About = () => {
   return (
     <div className="min-h-[90vh]">
-      <div className="flex font-poppins rounded-3xl text-white justify-center items-center mt-28          ">
+      <div className="flex font-poppins rounded-3xl text-white justify-center items-center mt-20 lg:mt-56 ">
         <div className="flex items-center md:flex-row flex-col lg:text-4xl md:text-3xl text-lg font-bold">
           <div className="flex justify-center leading-relaxed flex-col items-center">
             <Fade top cascade>
               <h1 className="text-3xl"> Tejas Nayak B</h1>
             </Fade>
-            <Bounce cascade>
-              <h6 className="text-xs">
+            <Bounce>
+              <h6 className="text-xs py-5 ml-[0.36rem] lg:ml-5 ">
                 CSE Undergrad • Web Developer • Tech Enthusiast
               </h6>
             </Bounce>
@@ -24,9 +24,9 @@ const Hero = () => {
         <div className="flex w-full  text-white justify-center">
           <div className="flex py-5 text-lg md:w-3/5 text-center">
             <Slide bottom cascade>
-              <p className="tracking-wide ">
+              <p className="  lg:tracking-wide font-[450] ">
                 Passionate web developer actively engaged in projects, and a
-                third-year engineering student at Sahyadri College of
+                third year engineering student at Sahyadri College of
                 Engineering, eager to explore new technologies.
               </p>
             </Slide>
@@ -37,4 +37,4 @@ const Hero = () => {
   );
 };
 
-export default Hero;
+export default About;
