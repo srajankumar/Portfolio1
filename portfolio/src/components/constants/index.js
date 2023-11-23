a9edd8edc41c2b8db5c26fde5bdb2b09920f123c
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-11-23 23:29:58 +0530

    Flowchart in progress

--- portfolio/src/components/constants/index.js
+++ portfolio/src/components/constants/index.js
@@ -1,92 +1,42 @@
 const events = [
   {
-    title: "Registrations open for Hacknight!",
+    title: "Became part of SOSC",
     description: "",
-    icon: `/assets/flowchart/registration.png`,
+    icon: `/assets/flowchart/SOSC-Logo.png`,
     iconBg: "#0f0913",
-    date: "17th October",
+    date: "March 2023 - Present",
     points: [
-      "Secure your spot at the most exhilarating tech event of Hacktober Fest to unleash your coding potential and work on some amazing projects!",
+      "Joined as Web Dev team member",
+      "Coordinator for an event UI Battle in college's National level Tech Fest via SOSC.",
+      "I served as a mentor for the junior batch's internship program, guiding them through a mini project based on C++.",
     ],
   },
   {
-    title: "Registration Deadline",
+    title: "Became part of IEI Chapter",
     description: "",
-    icon: `/assets/flowchart/deadline.png`,
+    icon: `/assets/flowchart/IEI-Logo.png`,
     iconBg: "#0f0913",
-    date: "28th October",
-    points: ["Form your teams and gear up for D-day!"],
-  },
-  {
-    title: "Hackathon Kick-off",
-    description: "",
-    icon: `/assets/flowchart/start.png`,
-    iconBg: "#0f0913",
-    date: "28th October - 11:00 a.m",
+    date: "April 2023 - Present",
     points: [
-      "Choose your fighter: pick a theme/track and start building on your concept!",
-      "You'll have a 24-hour window to transform your concept into a prototype.",
+      "Joined as Working committee secretary",
+      "Coordinated Cross-Platform Application Development Workshop",
     ],
   },
   {
-    title: "Hackathon Ends",
+    title: "Became part of GDSC SCEM",
     description: "",
-    icon: `/assets/flowchart/end.png`,
+    icon: `/assets/flowchart/GDSC-Logo.webp`,
     iconBg: "#0f0913",
-    date: "29 October - 11:00 a.m",
-    points: [
-      "Time's up!",
-      "Bid farewell to your coding marathons as the hackathon comes to a close. It's time to prepare for project presentations and the final showdown!",
-    ],
+    date: "August 2023 - Present",
+    points: ["Attended Devfest Mangalore"],
   },
   {
-    title: "Announcement of Results",
+    title: "Contact me !",
     description: "",
-    icon: `/assets/flowchart/trophy.png`,
+    icon: `/assets/flowchart/Gmail-Logo.png`,
     iconBg: "#0f0913",
-    date: "11th November",
-    points: [
-      "The moment of truth!",
-      "Get ready for the much-anticipated announcement of hackathon winners. Your hard work and innovation will be recognized, and prizes will be awarded.",
-    ],
+    points: [""],
   },
 ];
 
-const themes = [
-  {
-    name: "Healthcare",
-    image: "health.jpg",
-  },
-  {
-    name: "Finance",
-    image: "fin.jpg",
-  },
-  {
-    name: "Education",
-    image: "edu.jpg",
-  },
-  {
-    name: "Agriculture and Sustainability",
-    image: "agri.jpeg",
-  },
-];
-
-const judges = [
-  {
-    name: "Shailesh Shetty",
-    description: "Head of Department - DS, SIT",
-    src: "https://images.unsplash.com/photo-1682685796014-2f342188a635?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
-  },
-  {
-    name: "Varshaa Shetty",
-    description: "Vice President of SOSC",
-    src: "https://images.unsplash.com/photo-1604537466158-719b1972feb8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MTV8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-  },
-  {
-    name: "Tejas Nayak B",
-    description: "General Secretary of SOSC",
-    src: "https://images.unsplash.com/photo-1682685796014-2f342188a635?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=1770&q=80",
-  },
-];
-
-export { events, themes, judges };
+export { events };
